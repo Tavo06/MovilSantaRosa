4d06b2cf7049d00991979b429770b61596fadbc2
Kevin Flores <kevinnfloresp@gmail.com>
2026-07-05 13:59:08 -0500

    feat: resolve spacing

--- app/src/main/java/com/upsjb/movilsantarosa/core/navigation/MainNavHost.kt
+++ app/src/main/java/com/upsjb/movilsantarosa/core/navigation/MainNavHost.kt
@@ -103,7 +103,7 @@ fun MainNavHost(
 
         entry<FineFormDestination> {
             val viewModel: FineFormViewModel = hiltViewModel()
-            
+
             ResultEffect<Member> { member ->
                 viewModel.selectMember(member)
             }
