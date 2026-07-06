ae8cc32c0ddf3691b7a78925327b9346c2389bc7
Kevin Flores <kevinnfloresp@gmail.com>
2026-07-06 02:40:25 -0500

    feat: update socios

--- app/src/main/java/com/upsjb/movilsantarosa/feature/members/domain/repository/MemberRepository.kt
+++ app/src/main/java/com/upsjb/movilsantarosa/feature/members/domain/repository/MemberRepository.kt
@@ -1,8 +1,8 @@
 package com.upsjb.movilsantarosa.feature.members.domain.repository
 
 import com.upsjb.movilsantarosa.feature.members.domain.model.Member
+import kotlinx.coroutines.flow.Flow
 
 interface MemberRepository {
-    suspend fun getAllMembers():
-            Result<List<Member>>
+    fun getAllMembers(): Flow<List<Member>>
 }
